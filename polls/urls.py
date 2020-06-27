f6d1249299b8fcfe5814473ced795dd41a3ec31f
taharon <taharon1990@gmail.com>
2020-06-27 15:44:44 -0400

    first view created

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+  # so this path already has /polls/ from the tutorial_project urls.py. The views.index tells it to go to views
+  #   and take the index function I guess. not sure why I need hte name = index part
+  # from tutorial, path gets 4 arguments: route, view, [kwargs, name]
+  # route: the url pattern. I don't think this knows the difference between say a GET and a POST
+  # view: once django finds a matching pattern, it calls the specified view using the HttpRequest Object, and
+  #   any arguments from your URI as kwargs
+  # [kwargs] optional, a dictionary of arguments passed to the view
+  # [name]  optional, lets you name your view, which apparently lets you refer to it elsewhere
+  path('', views.index, name='index'),
+]
--- tutorial_project/urls.py
+++ tutorial_project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # this basically allows me to create urls in the polls.urls file that have the generic path /polls/something, with the /polls/ taken from here
+    path('polls/', include('polls.urls')),
 ]
