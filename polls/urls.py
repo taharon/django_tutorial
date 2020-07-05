c9ce1b0e81495532755ffb877e5cfffdd39fe956
taharon <taharon1990@gmail.com>
2020-07-05 15:49:39 -0400

    Added temporary api endpoints

--- polls/urls.py
+++ polls/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
   # [kwargs] optional, a dictionary of arguments passed to the view
   # [name]  optional, lets you name your view, which apparently lets you refer to it elsewhere
   path('', views.index, name='index'),
-
+  path('<int:question_id>/', views.detail, name='detail'),
+  path('<int:question_id>/results/', views.results, name='results'),
+  path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
