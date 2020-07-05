4cff6d15172aa8c2d54d385f4d818d6fe813bc72
taharon <taharon1990@gmail.com>
2020-07-05 16:25:41 -0400

    first set of views returned by endpoints

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
   # so this path already has /polls/ from the tutorial_project urls.py. The views.index tells it to go to views
   #   and take the index function I guess. not sure why I need hte name = index part
   # from tutorial, path gets 4 arguments: route, view, [kwargs, name]
@@ -11,8 +10,10 @@ urlpatterns = [
   #   any arguments from your URI as kwargs
   # [kwargs] optional, a dictionary of arguments passed to the view
   # [name]  optional, lets you name your view, which apparently lets you refer to it elsewhere
+app_name = 'polls'
+urlpatterns = [
   path('', views.index, name='index'),
   path('<int:question_id>/', views.detail, name='detail'),
   path('<int:question_id>/results/', views.results, name='results'),
-  path('<int:question_id>/vote/', views.vote, name='vote'),
+  path('<int:question_id>/vote/', views.vote, name='vote'),  
 ]
